e718e88d8e0bf2cc68dce6e58af677a6d9971dc5
belgonzalez21 <nsbelg@gmail.com>
2022-09-12 22:40:41 -0300

    enlazando react

---
+++ dashboard/src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+/*importación de componentes*/
+import SideBar from './SideBar';
+import ContentWrapper from './ContentWrapper';
+import GenresInDb from './GenresInDb'
+import LastMovieInDb from './LastMovieInDb';
+import ContentRowMovies from './ContentRowMovies';
+import Error from './Error';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom' 
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      	<div id="wrapper">
+          <SideBar />
+          <Routes>
+            <Route path='/' exact element={<ContentWrapper />}/>
+            <Route path='/genres' element={<GenresInDb />}/>
+            <Route path='/lastMovie' element={<LastMovieInDb />}/>
+            <Route path='/Movies' element={<ContentRowMovies />}/>
+            <Route path='*' element={<Error/>}/>
+          </Routes>
+        </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ dashboard/src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 className='h3 mb-0 font-weight-bold text-gray-800'>Error 404 - Archivo no encontrado</h1>
+        </div>
+    );
+}
+
+export default Error;
